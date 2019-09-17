7758b302f579809c419e67c2f0ae31691b28cd54
songdongdong2 <songdongdong@jd.com>
2019-09-17 13:08:40 +0800

    累加器学习

---
+++ src/main/scala/com/sjyttkl/bigdata/spark/Spark20_ShareData.scala
@@ -0,0 +1,47 @@
+package com.sjyttkl.bigdata.spark
+
+import java.sql.PreparedStatement
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.util.LongAccumulator
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+/**
+  * Create with: com.sjyttkl.bigdata.spark
+  * author: sjyttkl
+  * E-mail: 695492835@qq.com
+  * date: 2019/9/8 11:34
+  * version: 1.0
+  * description: 累加器
+  */
+object Spark20_ShareData {
+  def main(args: Array[String]): Unit = {
+
+    val config: SparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+
+    //创建spark上下文对象
+    val sc = new SparkContext(config)
+
+    val dataRDD:RDD[Int] = sc.makeRDD(List(1,2,3,4),2)
+
+    var sum :Int  = 0
+    //使用累加器来共享变量，来累加数据
+
+    //创建累加器对象
+    var accumulator: LongAccumulator = sc.longAccumulator
+    
+    dataRDD.foreach {
+      case i => {
+        //执行累加器的累加功能
+        accumulator.add(i)
+      }
+    }
+    //获取累加器的值
+    print("sum =" +accumulator.value)
+    //释放资源
+    sc.stop()
+  }
+
+
+}
